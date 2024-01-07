66ed07ce02ff29676f9df931bc49103e66602c72
Aidarkg <adijamgyrchiev@gmail.com>
2024-01-07 20:57:08 +0600

    deploy

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 PROXY_URL = 'http://proxy.server:3128'
 storage = MemoryStorage()
 TOKEN = config("TOKEN")
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot=bot, storage=storage, proxy=PROXY_URL)
+bot = Bot(token=TOKEN, proxy=PROXY_URL)
+dp = Dispatcher(bot=bot, storage=storage)
 GROUP_ID = config("GROUP_ID")
 GROUP_ID2 = config("GROUP_ID2")
 ADMIN_ID = config("ADMIN_ID")
